f96449a2dd9d925a94e8efd4da356688f83c8440
4lvin <phienal181@gmail.com>
2018-03-12 21:46:17 +0700

    initial commit

---
+++ app/src/main/java/com/woles/woles/woles/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.woles.woles.woles;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alvin on 12/03/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+/*
+        ButterKnife.bind(this);
+        sessionManager = SessionManager.getInstance();
+        if (sessionManager.isLoggedIn()) openMain();
+*/
+
+
+    }
+
+
+}
